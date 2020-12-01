b1bfe75f08125c3e94c88521a86b32321e21d774
Ji-Ha <xodhkd36@gmail.com>
2020-12-01 14:16:39 +0900

    SRP practice2

--- Precourse/src/SOLID_exam/Account.java
+++ Precourse/src/SOLID_exam/Account.java
@@ -1,7 +1,6 @@
 package SOLID_exam;
 
-//현재는 SRP에 어긋나는 코드다.
-//sum과 subtract는 변경을 관리하는 Production에 더하기 빼기 까지 있기 때문에 SRP에 어긋나는 코드.
+// 지금은
 //여기서 일급컬렉션을 적용하는건 잘못된건가?(price 값 변경이 필요하기 때문에?)
 public class Account {
 	private static final int MINIMUM_PRICE = 1000;
@@ -14,17 +13,22 @@ public class Account {
 	}
 	public void updatePrice(int price){
 		validatePrice(price);
-		this.price = price;
+		this.price += price;
 	}
 	private void validatePrice(int price){
 		if(price < MINIMUM_PRICE){
 			throw new IllegalArgumentException(String.format("최소가격은 %d원 이상입니다.", MINIMUM_PRICE));
 		}
 	}
-	public void sum(int price){
-		this.price+=price;
+}
+
+//값을 더하고 빼는 과정을 다른 클래스로 설정하면서 SRP를 실행하려 해보았다.
+//값을 변경하는 저 updatePrice를 빼고는 싶은데, 이건 더 생각해보아야겠다.
+class Calculator{
+	public void sum(Account a, int price){
+		a.updatePrice(price);
 	}
-	public void subtract(int price){
-		this.price-=price;
+	public void subtract(Account a, int price){
+		a.updatePrice(-price);
 	}
 }
