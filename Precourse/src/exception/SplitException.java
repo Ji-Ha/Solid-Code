d9e6c4408126461d2cf278f94bcc4a804963087f
Ji-Ha <xodhkd36@gmail.com>
2020-12-07 18:12:34 +0900

    scanner practice

---
+++ Precourse/src/exception/SplitException.java
@@ -0,0 +1,12 @@
+package exception;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class SplitException {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		System.out.println(Arrays.toString(scanner.next().split(",| , ")));
+	}
+}
