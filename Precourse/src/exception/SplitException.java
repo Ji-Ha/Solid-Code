baf6ae4044fe447e66e37e83fd95fa85f716fe1f
Ji-Ha <xodhkd36@gmail.com>
2020-12-07 18:15:21 +0900

    test: 들여쓰기 test

--- Precourse/src/exception/SplitException.java
+++ Precourse/src/exception/SplitException.java
@@ -8,5 +8,6 @@ public class SplitException {
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
 		System.out.println(Arrays.toString(scanner.next().split(",| , ")));
+
 	}
 }
